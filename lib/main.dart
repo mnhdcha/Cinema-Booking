b5a09f1d7dc3c5f226927af01e7c608eebe1aa44
mnhdcha <88828626+mnhdcha@users.noreply.github.com>
2024-09-24 22:40:58 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_design/Cinema%20App%20UI/pages/cinema_main_screen.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // Initialize Hive and open a box called 'MyBox'
-  await Hive.initFlutter();
-  await Hive.openBox("MyBox");
-
+void main() {
   runApp(const MyApp());
 }
 
